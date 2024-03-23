ed2fc7a40698dfe8928735e9c63d2d13c37a3a54
Guilherme Menezes <100319793+ms-guilherme@users.noreply.github.com>
2024-03-22 21:32:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function _Compare() {
     let section = document.getElementById("_comparison");
     section.style.display = "block";
-
-
-    let rect = section.getBoundingClientRect();
-    let scroll_destination = window.scrollY + rect.top + rect.height/2;
-
-    window.scrollTo({
-        top: scroll_destination,
-        behavior: "smooth"
-    })
-
     
     let our = document.getElementById("_our-num");
     let other = document.getElementById("_other-num");
@@ -18,6 +8,7 @@ function _Compare() {
 
     let our_num = document.getElementById('_initial-value').value;
     let other_num = document.getElementById('_initial-value').value;
+    let total = [0, 0];
 
     let current_date = new Date();
     let year_num = current_date.getFullYear();
@@ -33,6 +24,7 @@ function _Compare() {
     let alpha_our = 6;
     for(let i = 1; i < 15; i++) {
         our_num = our_num * 1.019;
+        total[0] += our_num;
         alpha_our += 4;
         our.innerHTML += `<p class = "_our-num" style="background-color: #50B37D${alpha_our};">$${our_num.toFixed(2)}</p>`;
         
@@ -43,12 +35,17 @@ function _Compare() {
     let alpha_other = 6;
     for(let i = 1; i < 15; i++) {
         other_num = other_num * 1.08;
+        total[1] += other_num;
         alpha_other += 4;
         other.innerHTML += `<p class = "_other-num" style="background-color: #b35050${alpha_other};">$${other_num.toFixed(2)}</p>`;
     }
 
 
     let _finalmessage = document.getElementById("_final-message");
-    let _saving = (other_num - our_num).toFixed(2);
+    let _saving = (total[1] - total[0]).toFixed(2);
     _finalmessage.innerHTML = 'By the year of ' + year_num + ', you would had <b>saved $' + _saving + '</b> with Terra Energy. <br> <a href="https://terraenergy.io/contact">Go solar now! Contact Us!</a>';
+
+    document.getElementById('_titles').scrollIntoView({
+        behavior: 'smooth'
+    });
 }
